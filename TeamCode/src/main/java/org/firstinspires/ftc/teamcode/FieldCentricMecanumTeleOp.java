70fecd7aa71bb4f7474ddbcaf1de2d33dc392e09
AcalanesRobotics <AcaBots7686@gmail.com>
2023-11-09 11:38:21 -0800

    right stick heading test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricMecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricMecanumTeleOp.java
@@ -6,11 +6,19 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
-public class FieldCentricMecanumTeleOp extends LinearOpMode{
+@TeleOp
+public class FieldCentricMecanumTeleOp extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
     @Override
     public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
         // Declare our motors
         // Make sure your ID's match your configuration
         DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
@@ -28,6 +36,7 @@ public class FieldCentricMecanumTeleOp extends LinearOpMode{
         // Retrieve the IMU from the hardware map
         IMU imu = hardwareMap.get(IMU.class, "imu");
         // Adjust the orientation parameters to match your robot
+        // Adjust the orientation parameters to match your robot
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.UP,
                 RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
@@ -41,7 +50,8 @@ public class FieldCentricMecanumTeleOp extends LinearOpMode{
         while (opModeIsActive()) {
             double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
             double x = gamepad1.left_stick_x;
-            double rx = gamepad1.right_stick_x;
+            double rx = -gamepad1.right_stick_x;
+            double r = Math.atan2(gamepad1.left_stick_x, gamepad1.left_stick_y);
 
             // This button choice was made so that it is hard to hit on accident,
             // it can be freely changed based on preference.
@@ -61,16 +71,21 @@ public class FieldCentricMecanumTeleOp extends LinearOpMode{
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio,
             // but only if at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-            double frontLeftPower = (rotY + rotX + rx) / denominator;
-            double backLeftPower = (rotY - rotX + rx) / denominator;
-            double frontRightPower = (rotY - rotX - rx) / denominator;
-            double backRightPower = (rotY + rotX - rx) / denominator;
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX), 1);
+            double frontLeftPower = (rotY + rotX) / denominator;
+            double backLeftPower = (rotY - rotX) / denominator;
+            double frontRightPower = (rotY - rotX) / denominator;
+            double backRightPower = (rotY + rotX) / denominator;
 
             frontLeftMotor.setPower(frontLeftPower);
             backLeftMotor.setPower(backLeftPower);
             frontRightMotor.setPower(frontRightPower);
             backRightMotor.setPower(backRightPower);
+
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Status", "Angle: "+botHeading);
+            telemetry.addData("Status", "Right Stick: "+r);
+            telemetry.update();
         }
     }
 }
