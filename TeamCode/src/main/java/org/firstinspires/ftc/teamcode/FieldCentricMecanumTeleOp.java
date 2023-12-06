4687222d6af53dd3fb5333d538b73277568edca7
AcalanesRobotics <AcaBots7686@gmail.com>
2023-12-06 11:05:18 -0800

    Added test to omni bot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricMecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricMecanumTeleOp.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.arcrobotics.ftclib.controller.PIDController;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
@@ -14,10 +15,16 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 public class FieldCentricMecanumTeleOp extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
+    private PIDController controller;
+    public static double p = 3, i = 0, d = 0;
+
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
+
+        controller = new PIDController(p,i,d);
+        double target = 0;
 
         // Declare our motors
         // Make sure your ID's match your configuration
@@ -51,6 +58,11 @@ public class FieldCentricMecanumTeleOp extends LinearOpMode {
             double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
             double x = gamepad1.left_stick_x;
             double rx = -gamepad1.right_stick_x;
+            double r = Math.atan2(gamepad1.right_stick_x, gamepad1.right_stick_y);
+
+            double prevTarget = 0;
+
+            controller.setPID(p,i,d);
 
             // This button choice was made so that it is hard to hit on accident,
             // it can be freely changed based on preference.
@@ -61,6 +73,10 @@ public class FieldCentricMecanumTeleOp extends LinearOpMode {
 
             double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
 
+            if((botHeading+0.01 >= r || botHeading-0.01 <= r) && (gamepad1.right_stick_x > 0 || gamepad1.right_stick_y> 0)){
+                target = controller.calculate(botHeading, r);
+            }
+
             // Rotate the movement direction counter to the bot's rotation
             double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
             double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
@@ -70,11 +86,11 @@ public class FieldCentricMecanumTeleOp extends LinearOpMode {
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio,
             // but only if at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-            double frontLeftPower = (rotY + rotX + rx) / denominator;
-            double backLeftPower = (rotY - rotX + rx) / denominator;
-            double frontRightPower = (rotY - rotX - rx) / denominator;
-            double backRightPower = (rotY + rotX - rx) / denominator;
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(target), 1);
+            double frontLeftPower = (rotY + rotX + target) / denominator;
+            double backLeftPower = (rotY - rotX + target) / denominator;
+            double frontRightPower = (rotY - rotX - target) / denominator;
+            double backRightPower = (rotY + rotX - target) / denominator;
 
             frontLeftMotor.setPower(frontLeftPower);
             backLeftMotor.setPower(backLeftPower);
@@ -82,7 +98,9 @@ public class FieldCentricMecanumTeleOp extends LinearOpMode {
             backRightMotor.setPower(backRightPower);
 
             telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Status", "Angle: "+botHeading);
+            telemetry.addData("Status", "Angle: "+((botHeading*180)/Math.PI));
+            telemetry.addData("Status", "Right Stick: "+((r*180)/Math.PI));
+            telemetry.addData("Status", "PID: "+((target*180)/Math.PI));
             telemetry.update();
         }
     }
